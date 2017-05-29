b537b7b1a6578843199ceff1893b8e71dcc9aa5c
hyperbeastcsgo <akharap92@gmail.com>
2017-05-29 15:45:14 +0700

    Initial commit

---
+++ src/calculater.php
@@ -0,0 +1,23 @@
+<?php
+    namespace hyperbeastcsgo\calculater;
+    class Cals 
+    {
+        public function add($a,$b)
+        {
+                return $a+$b;
+        }
+        public function del($a,$b)
+        {
+                return $a-$b;
+        }
+        public function mutiply($a,$b)
+        {
+                return $a*$b;
+        }
+        public function sub($a,$b)
+        {
+                return $a/$b;
+        }
+
+    }
+?>
